92f7545636a3d64875016ad52c025b0d0db13d6a
Musa OCAK <136326424+musaocak1@users.noreply.github.com>
2024-02-10 01:48:23 +0300

    2ndBreak

--- _01_MaskeTakip/WorkAround/Program.cs
+++ _01_MaskeTakip/WorkAround/Program.cs
@@ -2,9 +2,46 @@
 
 using System;
 
-string mesaj = "Selam";
-double tutar = 100000; // db den gelir
+SelamVer("Musa");
+SelamVer();
+int sonuc = Topla(3,5);
 
-Console.WriteLine(tutar*1.18);
-Console.WriteLine(tutar * 1.18);
 Console.ReadLine();
+
+static void SelamVer(string isim = "isimsiz")
+{
+    Console.WriteLine("Merhaba " + isim);
+}
+
+static int Topla(int sayi1 = 5 , int sayi2 = 10)
+{
+    int sonuc = sayi1 + sayi2;
+    Console.WriteLine("Toplam: " + sonuc.ToString());
+    return sonuc;
+}
+
+
+static void Degiskenler()
+{
+    string mesaj = "Selam";
+    double tutar = 100000; // db den gelir
+    int sayi = 100;
+    bool GirisYapmisMi = false;
+
+    string ad = "Musa";
+    string soyad = "Ocak";
+    int dogumYili = 1998;
+    long tcNO = 12345678910;
+
+    Console.WriteLine(tutar * 1.18);
+    Console.WriteLine(tutar * 1.18);
+}
+
+//pascal casing
+public class Vatandas
+{
+    public string Ad { get; set; }
+    public string Soyad { get; set; }
+    public int DogumYili { get; set; }
+    public long TcNo { get; set; }
+}
