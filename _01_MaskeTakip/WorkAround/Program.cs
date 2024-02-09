1464be9bda134bfb34369b1a88af6d5255a7ec61
Musa OCAK <136326424+musaocak1@users.noreply.github.com>
2024-02-10 00:57:43 +0300

    HW2 Began

---
+++ _01_MaskeTakip/WorkAround/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System;
+
+string mesaj = "Selam";
+double tutar = 100000; // db den gelir
+
+Console.WriteLine(tutar*1.18);
+Console.WriteLine(tutar * 1.18);
+Console.ReadLine();
